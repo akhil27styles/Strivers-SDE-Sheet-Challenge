408ed6ae22c8717d403b22b465084d43006ad4ea
Akhil Kumar Singh <akhil_kumar_singh@epam.com>
2023-06-14 23:49:29 +0530

    Day-7

---
+++ Day-7(LinkedList_Arrays)/trappingRainWater.java
@@ -0,0 +1,29 @@
+public class trappingRainWater {
+     public static long getTrappedWater(long[] arr, int n) {
+        // Write your code here.
+        long maxLeft=0,maxRight=0;
+        int left=0,right=n-1;
+        long res=0;
+        while(left<=right){
+            if(arr[left]<=arr[right]){
+                if(arr[left]>=maxLeft){
+                    maxLeft=arr[left];
+                }
+                else{
+                    res+=maxLeft-arr[left];
+                }
+                left++;
+            }
+            else{
+                if(arr[right]>=maxRight){
+                    maxRight=arr[right];
+                }
+                else{
+                    res+=maxRight-arr[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
+}
